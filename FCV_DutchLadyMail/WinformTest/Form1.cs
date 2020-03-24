1d1486e6e77933cb6bcd019f596cc47e1009aec7
dongta <dongnv.it@gmail.com>
2020-03-24 14:34:15 +0700

    d

---
+++ FCV_DutchLadyMail/WinformTest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using IVG.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DomainProcess dop = new DomainProcess();
+            List<string> a = dop.getUserDomain1("dongnv");
+            string user = string.Empty;
+            foreach (string item in a)
+            {
+                user += " " + item + ",";
+            }
+            MessageBox.Show(user);
+        }
+    }
+}
---
+++ FCV_DutchLadyMail/WinformTest/Helpers/DomainProcess.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Web;
+
+namespace IVG.Helpers
+{
+    public class DomainProcess
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private IEnumerable<string> GetDomains()
+        {
+            ICollection<string> domains = new List<string>();
+
+            // Querying the current Forest for the domains within.
+            foreach (Domain d in Forest.GetCurrentForest().Domains)
+                domains.Add(d.Name);
+
+            return domains;
+        }
+
+        private string GetDomainFullName(string friendlyName)
+        {
+            DirectoryContext context = new DirectoryContext(DirectoryContextType.Domain, friendlyName);
+            Domain domain = Domain.GetDomain(context);
+            return domain.Name;
+        }
+
+        private readonly string domain = "192.168.0.1";
+        public List<string> getUserDomain1(string userName)
+        {
+            log.Info("hello");
+            List<string> li = new List<string>();
+            foreach (string d in GetDomains())
+                // From the domains obtained from the Forest, we search the domain subtree for the given userName.
+                //using (DirectoryEntry domain = new DirectoryEntry(GetDomainFullName(d)))
+                using (DirectoryEntry domain = new DirectoryEntry(GetDomainFullName(d)))
+                {
+                    using (DirectorySearcher searcher = new DirectorySearcher())
+                    {
+                        searcher.SearchRoot = domain;
+                        searcher.SearchScope = SearchScope.Subtree;
+                        searcher.PropertiesToLoad.Add("sAMAccountName");
+                        // The Filter is very important, so is its query string. The 'objectClass' parameter is mandatory.
+                        // Once we specified the 'objectClass', we want to look for the user whose login
+                        // login is userName.
+                        searcher.Filter = string.Format("(&(objectClass=user)(sAMAccountName={0}))", userName);
+
+                        try
+                        {
+                            SearchResultCollection results = searcher.FindAll();
+
+                            // If the user cannot be found, then let's check next domain.
+                            if (results == null || results.Count == 0)
+                                continue;
+
+                            // Here, we yield return for we want all of the domain which this userName is authenticated.
+                            li.Add( domain.Path);
+                        }
+                        finally
+                        {
+                            searcher.Dispose();
+                            domain.Dispose();
+                        }
+                    }
+                }
+            return li;
+        }
+        public IEnumerable<string> GetUserDomain(string userName)
+        {
+            foreach (string d in GetDomains())
+                // From the domains obtained from the Forest, we search the domain subtree for the given userName.
+                //using (DirectoryEntry domain = new DirectoryEntry(GetDomainFullName(d)))
+                using (DirectoryEntry domain = new DirectoryEntry(GetDomainFullName(d)))
+                {
+                    using (DirectorySearcher searcher = new DirectorySearcher())
+                    {
+                        searcher.SearchRoot = domain;
+                        searcher.SearchScope = SearchScope.Subtree;
+                        searcher.PropertiesToLoad.Add("sAMAccountName");
+                        // The Filter is very important, so is its query string. The 'objectClass' parameter is mandatory.
+                        // Once we specified the 'objectClass', we want to look for the user whose login
+                        // login is userName.
+                        searcher.Filter = string.Format("(&(objectClass=user)(sAMAccountName={0}))", userName);
+
+                        try
+                        {
+                            SearchResultCollection results = searcher.FindAll();
+
+                            // If the user cannot be found, then let's check next domain.
+                            if (results == null || results.Count == 0)
+                                continue;
+
+                            // Here, we yield return for we want all of the domain which this userName is authenticated.
+                            yield return domain.Path;
+                        }
+                        finally
+                        {
+                            searcher.Dispose();
+                            domain.Dispose();
+                        }
+                    }
+                }
+        }
+    }
+}
